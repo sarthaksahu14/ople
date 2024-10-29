4ad77e51f671a8f9a72c2a49d4ed13d660500001
Sarthak Sahu <14sarthak.sahu@gmail.com>
2024-10-29 12:26:07 +0530

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/ople/", // Replace <repo-name> with your GitHub repository name
+  build: {
+    outDir: 'docs',
+  },
+});
